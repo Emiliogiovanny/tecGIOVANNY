e4df84436d8d5a8a049a7d916fcc20c64deb36e2
Integradora <a3522110482@alumno.uttehuacan.edu.mx>
2024-02-15 22:23:16 -0600

    primer parcialsuperadoJAJA

---
+++ src/components/Carga/Carga.js
@@ -0,0 +1,13 @@
+// Carga.js
+import React from 'react';
+import './Carga.css'; // Importa el archivo CSS
+
+const Carga = () => {
+  return (
+    <div className="carga-container">
+      CARGANDOO...
+    </div>
+  );
+}
+
+export default Carga;
